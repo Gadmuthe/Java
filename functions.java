15faa3eb6fe3b6293e55f89cf767c02a939fee7f
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2024-01-27 14:31:25 +0530

    Add files via upload

---
+++ functions.java
@@ -0,0 +1,10 @@
+public class FunctionExample {
+    public static void main(String[] args) {
+        int result = addNumbers(5, 10);
+        System.out.println("Sum: " + result);
+    }
+
+    static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
